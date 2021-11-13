18905492549d7cf8e83f5901b33cf0a5657811b6
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-13 20:59:10 +0800

    Program Added Some Python Colors FINISHED!

--- LIFESTAGES.py
+++ LIFESTAGES.py
@@ -1,14 +1,14 @@
 def age():
-    age = int(input('Please enter your age: '))
+    age = int(input('Please specify your \033[35m\033[1mage\033[0m\033[0m: '))
     if age >-1 and age <= 12:
-        print('KID!')
+        print('\033[1m\033[91m\033[4mKID!\033[0m\033[0m\033[0m')
     elif age >= 13 and age <= 17:
-        print('TEEN!')
+        print('\033[1m\033[91m\033[4mTEEN!\033[0m\033[0m\033[0m')
     elif age == 18:
-        print('DEBUT!')
+        print('\033[1m\033[91m\033[4mDEBUT!\033[0m\033[0m\033[0m')
     else:
-        print('ADULT!')
+        print('\033[1m\033[91m\033[4mADULT!\033[0m\033[0m\033[0m')
 def display():
-    print('>> PROGRAM FINISHED! <<')
+    print('\033[1m\033[94m>> DONE!!! <<\033[0m\033[0m')
 age()
 display()
